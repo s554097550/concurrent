d8a39132e74851a3078e4b6b96830eedb13183ac
swide <554097550@qq.com>
2019-10-07 15:30:20 +0800

    '增加测试controller和并发测试类。添加lombok插件'

---
+++ src/test/java/com/swd/concurrent/ConcurrentTest.java
@@ -0,0 +1,45 @@
+package com.swd.concurrent;
+
+import com.swd.concurrent.annoations.ThreadUnSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+@Slf4j
+@ThreadUnSafe
+public class ConcurrentTest {
+    // 请求总数
+    private static int clientTotal = 5000;
+    // 线程并发数
+    private static int concurrentNum = 200;
+
+    private static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        Semaphore semaphore = new Semaphore(concurrentNum);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count);
+    }
+
+    private static void  add(){
+        count ++;
+    }
+}
